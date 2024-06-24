d176b5d4f1d198768d03f1bb06f756b534a4d69e
Bo-Xiang Lai <imugly1029@gmail.com>
2024-06-24 23:25:56 +0800

    Add Fan control and OffTimer

--- README.md
+++ README.md
@@ -14,3 +14,9 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+## Module
+* [NodeMCU with ESP8266](https://github.com/BurkeyLai/ArduinoHub/tree/main/DaikinAirConditioner/TurnOnOffDaikinACByWifi)
+
+## Misc
+* [Icon Setup](https://pub.dev/packages/flutter_launcher_icons)
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 
 void main() {
@@ -27,11 +30,14 @@ class ControlPanel extends StatefulWidget {
 class _ControlPanelState extends State<ControlPanel> {
   final String baseUrl = 'http://192.168.1.110';  // 替换为你的ESP8266的IP地址
   String temperature = 'Loading...';
+  String fan = 'Loading...';
+  int offTimerHours = 0, offTimerMinutes = 0;
 
   @override
   void initState() {
     super.initState();
     fetchTemperature();
+    fetchFan();
   }
 
   Future<void> sendRequest(String endpoint) async {
@@ -43,6 +49,9 @@ class _ControlPanelState extends State<ControlPanel> {
       );
       if (endpoint == 'temp-up' || endpoint == 'temp-down') {
           fetchTemperature(); // 更新温度显示
+      }
+      if (endpoint == 'fan-up' || endpoint == 'fan-down') {
+          fetchFan(); // 更新温度显示
       }
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
@@ -65,14 +74,204 @@ class _ControlPanelState extends State<ControlPanel> {
       }
     } catch (e) {
       setState(() {
-        temperature = 'Error: $e';
+        // temperature = 'Error: $e';
+        temperature = '';
       });
+    }
+  }
+
+  Future<void> fetchFan() async {
+    try {
+      final response = await http.get(Uri.parse('$baseUrl/get-fan'));
+      if (response.statusCode == 200) {
+        setState(() {
+          fan = response.body;
+        });
+      } else {
+        setState(() {
+          fan = 'Error';
+        });
+      }
+    } catch (e) {
+      setState(() {
+        fan = '';
+      });
+    }
+  }
+
+  Future<void> sendOffTimer(int hour, int minute) async {
+    print("Hour: $hour, Minute: $minute");
+
+    final response = await http.get(Uri.parse('$baseUrl/set-off-timer?hour=$hour&minute=$minute'));
+
+    if (response.statusCode == 200) {
+      // print('Number sent successfully: ${response.body}');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Success: ${response.body}')),
+      );
+    } else {
+      print('Failed to send number');
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double tmpBtnWidth = screenWidth * 0.5;
+    double fanBtnWidth = screenWidth * 0.25;
+    double setOffTimerWidth = screenWidth * 0.25;
+    // final TextEditingController _hoursController = TextEditingController();
+    // final TextEditingController _minutesController = TextEditingController();
+
     return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Flexible(
+            flex: 1, // 按比例分配空间
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Fan: \n$fan',
+                  style: TextStyle(fontSize: 20),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 30),
+                SizedBox(
+                  width: fanBtnWidth, // 固定宽度
+                  child: ElevatedButton(
+                    onPressed: () => sendRequest('fan-up'),
+                    child: Icon(Icons.arrow_upward),
+                  )
+                ),
+                SizedBox(
+                  width: fanBtnWidth, // 固定宽度
+                  child: ElevatedButton(
+                    onPressed: () => sendRequest('fan-down'),
+                    child: Icon(Icons.arrow_downward),
+                  )
+                ),
+              ],
+            ),
+          ),
+          
+          SizedBox(width: 10), 
+          
+          Flexible(
+            flex: 2, // 按比例分配空间
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Temperature: \n$temperature°C',
+                  style: TextStyle(fontSize: 20),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 30),
+                SizedBox(
+                  width: tmpBtnWidth, // 固定宽度
+                  child: ElevatedButton(
+                    onPressed: () => sendRequest('on-off'),
+                    child: Text('Turn ON / OFF'),
+                  ),
+                ),
+                SizedBox(height: 30), // 增加间距
+                SizedBox(
+                  width: tmpBtnWidth, // 固定宽度
+                  child: ElevatedButton.icon(
+                    onPressed: () => sendRequest('temp-up'),
+                    icon: Icon(Icons.arrow_upward),
+                    label: Text('Increase'),
+                  ),
+                ),
+                SizedBox(
+                  width: tmpBtnWidth, // 固定宽度
+                  child: ElevatedButton.icon(
+                    onPressed: () => sendRequest('temp-down'),
+                    icon: Icon(Icons.arrow_downward),
+                    label: Text('Decrease'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          const SizedBox(width: 10), 
+
+          Flexible(
+            flex: 1, // 按比例分配空间
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Off After: \n',
+                  style: TextStyle(fontSize: 20),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  width: setOffTimerWidth,
+                  child: Column(
+                    children: [
+                      TextField(
+                        // controller: _hoursController,
+                        decoration: const InputDecoration(
+                          labelText: 'Hours',
+                          border: OutlineInputBorder(),
+                        ),
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly,
+                          LengthLimitingTextInputFormatter(2),
+                          FilteringTextInputFormatter.allow(RegExp(r'^([01]?[0-9]|2[0-3])$')),
+                        ],
+                        onChanged: (value) {
+                          setState(() {
+                            if (value.isNotEmpty) {
+                              offTimerHours = int.parse(value);
+                            } else {
+                              offTimerHours = 0;
+                            }
+                          });
+                        },
+                      ),
+                      const SizedBox(height: 10),
+                      TextField(
+                        // controller: _minutesController,
+                        decoration: const InputDecoration(
+                          labelText: 'Minutes',
+                          border: OutlineInputBorder(),
+                        ),
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly,
+                          LengthLimitingTextInputFormatter(2),
+                          FilteringTextInputFormatter.allow(RegExp(r'^[0-5]?[0-9]$')),
+                        ],
+                        onChanged: (value) {
+                          setState(() {
+                            if (value.isNotEmpty) {
+                              offTimerMinutes = int.parse(value);
+                            } else {
+                              offTimerMinutes = 0;
+                            }
+                          });
+                        },
+                      ),
+                      const SizedBox(height: 10),
+                      ElevatedButton(
+                        onPressed: () => sendOffTimer(offTimerHours, offTimerMinutes),
+                        child: const Text('Set'),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      /*
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -108,6 +307,7 @@ class _ControlPanelState extends State<ControlPanel> {
           ),
         ],
       ),
+      */
     );
   }
 }
